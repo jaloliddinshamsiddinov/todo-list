d8fb4db900922efcad76cfc720db6d4f359f979f
jaloliddinshamsiddinov <125132177+jaloliddinshamsiddinov@users.noreply.github.com>
2023-09-17 00:13:15 +0500

    fix/localstorage

--- js/app.js
+++ js/app.js
@@ -1,46 +1,84 @@
-let inps = document.querySelectorAll("#form input");
-let box = document.getElementById("box");
+const form = document.getElementById("form");
+const inputs = document.querySelectorAll("#form input");
+const btn = document.querySelector("#btn");
+const box = document.querySelector("#box");
+const editBtn = document.querySelector("#editBtn");
 
-function cleareInp() {
-  for (let i of inps) {
-    i.value = "";
-  }
-}
-
-let arr = [];
-btn.addEventListener("click", (e) => {
+let val = JSON.parse(localStorage.getItem("todo"));
+let arr = val ? val : [];
+console.log(arr);
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let obj = {};
-
-  for (let i of inps) {
-    obj[i.name] = i.value;
+  for (let i of inputs) {
+    if (i.value) {
+      obj[i.name] = i.value;
+    }
+    i.value = "";
   }
-
-  cleareInp();
   arr.push({ ...obj, id: Date.now() });
-  ui();
+  localStorage.setItem("todo", JSON.stringify(arr));
+  create();
 });
 
-let ui = () => {
-  box.innerHTML = "";
-  for (let i = 0; i < arr.length; i++) {
+const create = () => {
+  box.innerHTML = [];
+  for (let i of arr) {
     box.innerHTML += `
           <div class="box_item">
           <div class="content">
-              <h2 class="lName">${arr[i].fName}</h2>
-              <h2 class="fName">${arr[i].lName}</h2>
-              <p class="email">${arr[i].email}</p>
+              <h2 class="lName">${i.fName}</h2>
+              <h2 class="fName">${i.lName}</h2>
+              <p class="email">${i.email}</p>
           </div>
           <div class="UpDe">
-              <button>Edit</button>
-              <button onclick="${edit(this)}">Delete</button>
+              <button onclick="edit(${i.id})">Edit</button>
+              <button onclick="dlete(${i.id})">Delete</button>
           </div>
           </div>
           `;
   }
 };
+create();
+const dlete = (id) => {
+  for (let i of arr) {
+    if (i.id == id) {
+      arr = arr.filter((obj) => obj.id !== id);
+      console.log(arr);
+    }
+  }
+  localStorage.setItem("todo", JSON.stringify(arr));
+  create();
+};
 
-let edit = (e) => {
-  const update = document.querySelector(".UpDe")
-  console.log(update);
-}
+let idi = 0;
+const edit = (id) => {
+  idi = id;
+  btn.style.display = "none";
+  editBtn.style.display = "block";
+  for (let i of arr) {
+    if (i.id == id) {
+      for (let edit of inputs) {
+        edit.value = i[edit.name];
+      }
+    }
+  }
+  localStorage.setItem("todo", JSON.stringify(arr));
+};
+
+const editF = () => {
+  let id = idi;
+  box.innerHTML = "";
+  btn.style.display = "";
+  editBtn.style.display = "none";
+  for (let i of arr) {
+    if (i.id == id) {
+      for (let edit of inputs) {
+        i[edit.name] = edit.value;
+      }
+    }
+  }
+  localStorage.setItem("todo", JSON.stringify(arr));
+  create();
+  console.log(id);
+};
